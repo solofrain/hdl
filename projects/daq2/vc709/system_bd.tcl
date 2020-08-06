10de526ee52682d7cffb83aa75955b8ce039c574
Dan Hotoleanu <dan.hotoleanu@analog.com>
2020-08-06 07:59:30 +0100

    daq2: vc709: Added the vc709 carrier for the daq2 project
    Added new Virtex 7 vc709 carrier board. The main focus of the project is to
    make use of the GTH trancievers available on the Virtex 7 FPGA chip on the
    board. The vc709 is tested with the daq2 project.
    
    Signed-off-by: Dan Hotoleanu <dan.hotoleanu@analog.com>

---
+++ projects/daq2/vc709/system_bd.tcl
@@ -0,0 +1,4 @@
+source $ad_hdl_dir/projects/common/vc709/vc709_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/adcfifo_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+source ../common/daq2_bd.tcl
